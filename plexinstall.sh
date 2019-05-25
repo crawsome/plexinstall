d91f5f85822b4a3564c025cc1b6bd39ea581c93d
crawsome <c3g6b2@gmail.com>
2019-05-25 00:43:48 -0700

    Update plexinstall.sh

--- plexinstall.sh
+++ plexinstall.sh
@@ -1,1 +1,12 @@
 #1/bin/sh
+
+# check if plex service account exists. If not, make a plex service account
+
+#make plex director
+mkdir ~/plexdownload/ && cd $_
+
+#download plex 
+wget https://downloads.plex.tv/plex-media-server/1.15.6.1079-78232c603/plexmediaserver_1.15.6.1079-78232c603_amd64.deb
+
+dpkg -i plexmediaserver*.deb
+
